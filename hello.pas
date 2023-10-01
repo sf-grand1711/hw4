45f65c9f2653c7d8854e782acdec707feb657136
Грищук Александр <grishchuk@sfedu.ru>
2023-10-01 19:39:56 +0300

    Add hello.pas with greetings in different languages

--- hello.pas
+++ hello.pas
@@ -3,7 +3,8 @@
   println('Select your language');
   println('Wähle deine Sprache');
   println('Choisissez votre langue');
-  var lang := ReadInteger('1 - RU, 2 - EN, 3 - DE, 4 - FR:');
+  println('1 - RU, 2 - EN, 3 - DE, 4 - FR');
+  var lang := ReadInteger;
   Assert((lang > 0) and (lang < 5), 
   'Неверное значение, Incorrect value, Falscher Wert, Valeur incorrecte');
   case lang of
